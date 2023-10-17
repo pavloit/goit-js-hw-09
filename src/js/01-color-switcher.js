16f39f0e8816b85573fbac5563735410bb46ae30
pavloit <dba.devop@gmail.com>
2023-10-17 14:25:02 +0300

    hw09_task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+console.log('start: ', startBtn);
+console.log('stop: ', stopBtn);
+console.log('body: ', bodyEl);
+startBtn.addEventListener('click', onClickStart)
+stopBtn.addEventListener('click', onClickStop)
+let timerId = 1;
+
+function onClickStart() {
+    startBtn.setAttribute('disabled', 'true');
+    timerId = setInterval(changeBodyColor, 1000);
+}
+
+function onClickStop() {
+        startBtn.removeAttribute('disabled');
+        clearInterval(timerId);
+    }
+
+function changeBodyColor() {
+    bodyEl.style.backgroundColor= getRandomHexColor();
+}
+
+
 
